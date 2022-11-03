9a6853a34aabd6ab6e0e5a4cacf40b81b15cf402
alexandereubanks <alexandereubanks@localhost>
2022-11-02 20:50:31 -0400

    General Updates

---
+++ my.first.project/src/test/java/NewTest.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.Test;
+
+public class NewTest {
+  
+	@Test
+  public void canRunTestNGTest() {
+	  //Prerequisite
+  }
+  
+  
+  @Test
+  public void canUseVar() {
+	  String myString = "Alexander";
+	  var myVarzStrong = "Alex";
+  }
+}
